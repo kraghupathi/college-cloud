676cab1e00055b0b4f5680334556c3cbaef83018
SivaShanker <shankar@vlabs.ac.in>
2017-12-06 18:22:25 +0530

    Links update and update cpu, RAM  utilization

--- src/testing/performance-test/test-case2/32gb/test-case2-siege.sh
+++ src/testing/performance-test/test-case2/32gb/test-case2-siege.sh
@@ -1,14 +1,14 @@
-#############physical chemistry#############
-#siege -c 100 -r 1 -t 2M http://ccnsb06-iiith.vlabs.ac.in/exp6_10/IR_Powder_exp6.swf > siege-100-users-pc.txt 2>&1
-#sleep 10s
-#
-#siege -c 200 -r 1 -t 2M http://ccnsb06-iiith.vlabs.ac.in/exp6_10/IR_Powder_exp6.swf > siege-200-users-pc.txt 2>&1
-#sleep 10s
-#
-#siege -c 300 -r 1 -t 2M http://ccnsb06-iiith.vlabs.ac.in/exp6_10/IR_Powder_exp6.swf > siege-300-users-pc.txt 2>&1
-#sleep 10s
-#
-###############computer programming ###########
+############physical chemistry#############
+siege -c 100 -r 1 -t 2M http://ccnsb06-iiith.vlabs.ac.in/exp6_10/IR_Powder_exp6.swf > siege-100-users-pc.txt 2>&1
+sleep 10s
+
+siege -c 200 -r 1 -t 2M http://ccnsb06-iiith.vlabs.ac.in/exp6_10/IR_Powder_exp6.swf > siege-200-users-pc.txt 2>&1
+sleep 10s
+
+siege -c 300 -r 1 -t 2M http://ccnsb06-iiith.vlabs.ac.in/exp6_10/IR_Powder_exp6.swf > siege-300-users-pc.txt 2>&1
+sleep 10s
+
+##############computer programming ###########
 siege -c 100 -r 1 -t 2M http://cse02-iiith.vlabs.ac.in/exp4/simulation/2-D/index.html > siege-100-users-cp.txt 2>&1
 sleep 10s
 
@@ -18,12 +18,12 @@ sleep 10s
 siege -c 300 -r 1 -t 2M http://cse02-iiith.vlabs.ac.in/exp4/simulation/2-D/index.html > siege-300-users-cp.txt 2>&1
 sleep 10s
 
-###############vlsi lab ####################
-#siege -c 100 -r 1 -t 2M http://cse14-iiith.vlabs.ac.in/final-build/EXP_1sep2010/exp1/exp1.html > siege-100-users-vlsi.txt 2>&1
-#sleep 10s
-#
-#siege -c 200 -r 1 -t 2M http://cse14-iiith.vlabs.ac.in/final-build/EXP_1sep2010/exp1/exp1.html > siege-200-users-vlsi.txt 2>&1
-#sleep 10s
-#
-#siege -c 300 -r 1 -t 2M http://cse14-iiith.vlabs.ac.in/final-build/EXP_1sep2010/exp1/exp1.html > siege-300-users-vlsi.txt 2>&1
+##############vlsi lab ####################
+siege -c 100 -r 1 -t 2M http://cse14-iiith.vlabs.ac.in/final-build/EXP_1sep2010/exp1/exp1.html > siege-100-users-vlsi.txt 2>&1
+sleep 10s
+
+siege -c 200 -r 1 -t 2M http://cse14-iiith.vlabs.ac.in/final-build/EXP_1sep2010/exp1/exp1.html > siege-200-users-vlsi.txt 2>&1
+sleep 10s
+
+siege -c 300 -r 1 -t 2M http://cse14-iiith.vlabs.ac.in/final-build/EXP_1sep2010/exp1/exp1.html > siege-300-users-vlsi.txt 2>&1
 
