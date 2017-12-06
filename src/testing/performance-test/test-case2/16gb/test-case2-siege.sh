4414cb58bd11c9a06a89f7d78ee6d86b511166a0
SivaShanker <shankar@vlabs.ac.in>
2017-12-06 18:09:27 +0530

    Test cases for 16 GB RAM

--- src/testing/performance-test/test-case2/16gb/test-case2-siege.sh
+++ src/testing/performance-test/test-case2/16gb/test-case2-siege.sh
@@ -1,39 +1,29 @@
-#############physical chemistry#############
-siege -c 100 -r 1 -t 3M http://ccnsb06-iiith.vlabs.ac.in/exp6_10/IR_Powder_exp6.swf >> siege-100-users-pc.txt
-
-#########sleep 10sec########
-sleep 10s
-
-siege -c 200 -r 1 -t 3M http://ccnsb06-iiith.vlabs.ac.in/exp6_10/IR_Powder_exp6.swf >> siege-200-users-pc.txt
-#########sleep 10sec########
-sleep 10s
-
-siege -c 300 -r 1 -t 3M http://ccnsb06-iiith.vlabs.ac.in/exp6_10/IR_Powder_exp6.swf >> siege-300-users-pc.txt
-#########sleep 10sec########
-sleep 10s
-
-
+############physical chemistry#############
+#siege -c 100 -r 1 -t 2M http://ccnsb06-iiith.vlabs.ac.in/exp6_10/IR_Powder_exp6.swf > siege-100-users-pc.txt 2>&1
+#sleep 10s
+#
+#siege -c 200 -r 1 -t 2M http://ccnsb06-iiith.vlabs.ac.in/exp6_10/IR_Powder_exp6.swf > siege-200-users-pc.txt 2>&1
+#sleep 10s
+#
+#siege -c 300 -r 1 -t 2M http://ccnsb06-iiith.vlabs.ac.in/exp6_10/IR_Powder_exp6.swf > siege-300-users-pc.txt 2>&1
+#sleep 10s
+#
 ###############computer programming ###########
-siege -c 100 -r 1 -t 3M http://cse02-iiith.vlabs.ac.in/exp4/simulation.html >> siege-100-users-cp.txt
-#########sleep 10sec########
-sleep 10s
+#siege -c 100 -r 1 -t 2M http://cse02-iiith.vlabs.ac.in/exp4/simulation/2-D/index.html > siege-100-users-cp.txt 2>&1
+#sleep 10s
+#
+#siege -c 200 -r 1 -t 2M http://cse02-iiith.vlabs.ac.in/exp4/simulation/2-D/index.html > siege-200-users-cp.txt 2>&1
+#sleep 10s
+#
+#siege -c 300 -r 1 -t 2M http://cse02-iiith.vlabs.ac.in/exp4/simulation/2-D/index.html > siege-300-users-cp.txt 2>&1
+#sleep 10s
+#
+##############vlsi lab ####################
+#siege -c 100 -r 1 -t 2M http://cse14-iiith.vlabs.ac.in/final-build/EXP_1sep2010/exp1/exp1.html > siege-100-users-vlsi.txt 2>&1
+#sleep 10s
+#
+#siege -c 200 -r 1 -t 2M http://cse14-iiith.vlabs.ac.in/final-build/EXP_1sep2010/exp1/exp1.html > siege-200-users-vlsi.txt 2>&1
+#sleep 10s
 
-siege -c 200 -r 1 -t 3M http://cse02-iiith.vlabs.ac.in/exp4/simulation.html >> siege-200-users-cp.txt
-#########sleep 10sec########
-sleep 10s
-
-siege -c 300 -r 1 -t 3M http://cse02-iiith.vlabs.ac.in/exp4/simulation.html >> siege-300-users-cp.txt
-#########sleep 10sec########
-sleep 10s
-
-###############vlsi lab ####################
-siege -c 100 -r 1 -t 3M http://cse14-iiith.vlabs.ac.in/final-build/EXP_1sep2010/exp1/exp1.html >> siege-100-users-vlsi.txt
-#########sleep 10sec########
-sleep 10s
-
-siege -c 200 -r 1 -t 3M http://cse14-iiith.vlabs.ac.in/final-build/EXP_1sep2010/exp1/exp1.html >> siege-200-users-vlsi.txt
-#########sleep 10sec########
-sleep 10s
-
-siege -c 300 -r 1 -t 3M http://cse14-iiith.vlabs.ac.in/final-build/EXP_1sep2010/exp1/exp1.html >> siege-300-users-vlsi.txt
+siege -c 300 -r 1 -t 2M http://cse14-iiith.vlabs.ac.in/final-build/EXP_1sep2010/exp1/exp1.html > siege-300-users-vlsi.txt 2>&1
 
